792c81d1c626df49f5a5881d97564e69107aefb3
ジョン W. B <johnb@ciencias.unam.mx>
2022-10-09 18:19:58 -0500

    Add conexion WIFI with SP32

---
+++ Practica/WIFI&SPIFFS/Wifi_test.ino
@@ -0,0 +1,49 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <WiFi.h>
+
+const char* SSID     = "*****";
+const char* PASSWORD = "*****";
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+
+    WiFi.begin(SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    delay(10000);
+}
