1a6f2e25de2f514b236f6c0e990bb4a23eea8f7f
JohnKun136NVCP <73410627+JohnKun136NVCP@users.noreply.github.com>
2023-08-23 12:49:49 -0600

    [MAIN][OWNER][FIX] Se modifico el .ino de los leds de forma ascendente y descendente

--- Introduccion a IoT/LedsTurnOnOff/LedsTurnOnOff.ino
+++ Introduccion a IoT/LedsTurnOnOff/LedsTurnOnOff.ino
@@ -1,25 +1,30 @@
 //Definir el arreglo de leds
-
 const int leds[5] = {14, 27, 26, 25, 33}; 
 
 void setup() {
   //Definir los pines como salida
   for (int i = 0;i<=4;i++){
     pinMode(leds[i],OUTPUT);
+    
   }
-
 }
 
 void loop() {
   //Ciclo para enceder los leds uno por uno.
   for(int i = 0;i< 5;i++){
     digitalWrite(leds[i],HIGH);
-    delay(50);
-  }
-  //Ciclo para apagar los leds en modo descendente
-  for(int i = 5;i>=0;i--){
+    delay(20);
     digitalWrite(leds[i],LOW);
-    delay(50);
+    delay(20);
   }
-
+  //Un pequeño retardo.
+  delay(20);
+  //Ciclo para apagar los leds en modo descendente
+   for(int i = 4;i>=0;i--){
+    digitalWrite(leds[i],HIGH);
+    delay(20);
+    digitalWrite(leds[i],LOW);
+    delay(20);
+  }
+  delay(20);
 }
