fb1625a131a7b65a813b96ab103c80a3efff6973
JohnKun136NVCP <johnb@ciencias.unam.mx>
2023-11-13 00:16:31 -0600

    [MODIFICATED][PYTHONDEV][COLLABORATOR]
    - Comment code
    - Added to create a  hidden directory
    - Use absolute path to created

--- Introduccion a IoT/Proyecto/pyfiles/getips.py
+++ Introduccion a IoT/Proyecto/pyfiles/getips.py
@@ -2,30 +2,31 @@ import serial
 import serialpports as serport
 import time
 import os.path
-def getIPs():
+def getIPs(): #Get ip from esp32 by SERIAL
     ips = []
     try:
-        nameport = serport.ports()
-        esp = serial.Serial(nameport,115200)
-        print("REBOOT your ESP32 to get IPS")
-        while (len(ips)<1):
+        nameport = serport.ports() #Show ports
+        esp = serial.Serial(nameport,115200)#Port selected
+        print("REBOOT your ESP32 to get IPS")#Make sure a reboot to work
+        while (len(ips)<1):#Only send ip from esp32
             line = esp.readline().decode('utf-8').rstrip()
-            ips.append(line)
+            ips.append(line)#IP
         esp.close()
-        return ips
+        return ips #Return ip into a list
     except:
-        return []
+        return [] #If not connected or port is not available return []
 
-def generateTxtFile(ipSta):
-    path = '~/.ips/ips.txt'
+def generateTxtFile(ipSta): #Generate ip and make directory if it is necessary
+    path = os.path.abspath(os.getcwd())+"/.ip/ip.txt"
     if(os.path.isfile(path)!=True):
-        with open("ips.txt","a") as file:
+        os.mkdir(".ip");os.chdir(".ip/")
+        with open("ip.txt","a") as file:
             file.write(ipSta)
     else:
-        print("Do you want to overwrite it? [Y/N]")
-        option = input()
-        if(option=="Y" or option=="yes" or option == "YES"):
-            with open("ips.txt","w") as file:
+        os.chdir(".ip/")
+        option = input("Do you want to overwrite it? [Y/N]: ")
+        if(option=="Y" or option=="yes" or option == "YES" or option=="y"):
+            with open("ip.txt","w") as file:
                 file.write(ipSta)
         else:
             print("Nothing to do here")
@@ -38,7 +39,7 @@ while (True):
         print("Fist, connect your esp32 to port serial,please")
     elif(iplist!=[]):
         ipsta = iplist[0]
-        print("La IP publica STA es: ",ipsta)
+        print("Your IP STA is: ",ipsta)
         print("Now you can unplugged your ESP32")
         break
 generateTxtFile(ipsta)
