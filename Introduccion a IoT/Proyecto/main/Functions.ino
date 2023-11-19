d3104975c3744516330f19781620323404ca2e79
JohnKun136NVCP <johnb@ciencias.unam.mx>
2023-11-18 20:29:45 -0600

    [ADD][DEV][COLLABORATOR]
    - Removed message to get by Python file
    - Replaced Telegram logs by Warning or Danger messages
    - Added telegramBotServices to idsLedUP
    - Removed SMTP service
    - Deleted lostMessage varible and option

--- Introduccion a IoT/Proyecto/main/Functions.ino
+++ Introduccion a IoT/Proyecto/main/Functions.ino
@@ -7,10 +7,7 @@ void idsLedUP(){ //Function to connect a server to send information as WI
   while (client.connected()) {
     if (client.available()) {
       String request = client.readStringUntil('\n'); //Get 1,0 or -1 
-      String message = client.readStringUntil('\r'); //Get length of packet
       Serial.print(request);
-      Serial.print(",");
-      Serial.println(message);
       if(request=="0"){//IDS traffic it's OK
         ok++; //Add counter to ok traffic
         digitalWrite(LED_1,HIGH);
@@ -18,13 +15,15 @@ void idsLedUP(){ //Function to connect a server to send information as WI
         digitalWrite(LED_1,LOW);
       }
       else if (request=="1"){//IDS traffic, it's warning
+        warningMessage = true;dangerMessage=false;
+        senderMessageLog(warningMessage,dangerMessage); //Send logs to Telegram
         digitalWrite(LED_2,HIGH);
         delay(1000);
         digitalWrite(LED_2,LOW);
         warning++;//Add counter to warning traffic
-        warningMessage = true;dangerMessage=false;
-        senderMessageLog(warningMessage,dangerMessage); //Send logs to Telegram
       }else if(request=="-1"){ //IDS traffic, it's danger
+        warningMessage = false;dangerMessage=true;
+        senderMessageLog(warningMessage,dangerMessage); //Sends DANGER alert
         digitalWrite(LED_3,HIGH);
         delay(1000);
         digitalWrite(LED_3,LOW);
@@ -33,11 +32,9 @@ void idsLedUP(){ //Function to connect a server to send information as WI
         digitalWrite(Bzzer, HIGH);   // turn on the buzzer 
         delay(1000);                 // wait
         digitalWrite(Bzzer , LOW);  // turn off the buzzer
-        warningMessage = false;dangerMessage=true;
-        senderMessageLog(warningMessage,dangerMessage); //Sends DANGER alert
-        sendingEmail(message);//Send a email
       }
       client.stop();
+      telegramBotService();
     }
     pingTargets();//Generate traffic
   }
@@ -55,53 +52,6 @@ void pingTargets(){
     return;
   }
 }
-
-//SMTP -> Sends emails. Check documentation on https://github.com/mobizt/ESP-Mail-Client
-void sendingEmail(String messageIP){
-   smtp.debug(1);
-   smtp.callback(smtpCallback);
-   ESP_Mail_Session session;
-   session.server.host_name = SMTP_HOST;
-   session.server.port = SMTP_PORT;
-   session.login.email = AUTHOR_EMAIL;
-   session.login.password = AUTHOR_PASS;
-   SMTP_Message message;
-   message.sender.name = F("ESP IDS WARNING SECURITY LOGS");
-   message.sender.email = AUTHOR_EMAIL;
-   message.subject = F("SECURITY ALERT!!");
-   message.addRecipient(F("John"), RECIPIENT_EMAIL);
-   String textMsg = "ALERT.\nExcess traffic has been detected from IP "+(String)messageIP+"\nPlease review your intranet monitoring." ;
-   message.text.content = textMsg.c_str();
-   message.text.charSet = "us-ascii";  
-   message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
-   message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_high;
-  /* Connect to the server */
-  if (!smtp.connect(&session)){
-    //ESP_MAIL_PRINTF("Connection error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
-    return;
-  }
-  /* Start sending Email and close the session */
-  if (!MailClient.sendMail(&smtp, &message)){
-    //ESP_MAIL_PRINTF("Error, Status Code: %d, Error Code: %d, Reason: %s", smtp.statusCode(), smtp.errorCode(), smtp.errorReason().c_str());
-    return;
-  }
-}
-void smtpCallback(SMTP_Status status)
-{
-  if (status.success())
-  {
-    struct tm dt;
-    for (size_t i = 0; i < smtp.sendingResult.size(); i++)
-    {
-      /* Get the result item */
-      SMTP_Result result = smtp.sendingResult.getItem(i);
-      time_t ts = (time_t)result.timestamp;
-      localtime_r(&ts, &dt);
-    }
-    smtp.sendingResult.clear();
-  }
-}
-
 //Telegram Bots
 //Options functions for Telegram bot
 void logsMessages(String option){
@@ -127,16 +77,16 @@ void numberSendEmail(){
 }
 //Sends notifications to elegram
 void senderMessageLog(bool warningMessage, bool dangerMessage){
-  if((warningMessage==true) && (dangerMessage == false) &&(lostMessage == false)){
-    String messageStatus = "WARNING! A suspicious packet was detected on your network";
-    myBot.sendMessage(msg.sender.id, messageStatus);
-  }else if ((warningMessage==false) && (dangerMessage == true) && (lostMessage == false)){
-    String messageStatus = "DANGER! A issue packet was detected on your network";
-    myBot.sendMessage(msg.sender.id, messageStatus);
-  }else if  ((warningMessage==false) && (dangerMessage == false) && (lostMessage == true)){
-    String messageStatus = "A package was lost while the IDS was working";
-    myBot.sendMessage(msg.sender.id, messageStatus);
-  }
+  if((warningMessage==true) && (dangerMessage == false)){
+     String messageStatus = "WARNING! A suspicious packet was detected on your network";
+     myBot.sendMessage(msg.sender.id, messageStatus);
+  }else if ((warningMessage==false) && (dangerMessage == true)){
+     String messageStatus = "DANGER! A issue packet was detected on your network";
+     myBot.sendMessage(msg.sender.id, messageStatus);
+    }else if  ((warningMessage==false) && (dangerMessage == false)){
+      String messageStatus = "A package was lost while the IDS was working";
+      myBot.sendMessage(msg.sender.id, messageStatus);
+    }
 }
 
 //Telegram bot main
