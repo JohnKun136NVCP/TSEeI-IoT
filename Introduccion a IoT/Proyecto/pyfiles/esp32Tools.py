befe166ef65fed75a8e7c70b9210a948aba9234a
JohnKun136NVCP <johnb@ciencias.unam.mx>
2023-11-18 20:36:01 -0600

    [MODIFICATED][PYTHONDEV][COLLABORATOR]
    - Added SMTP services
    - Replaced information
    - Class Email
    - Credentials is hidden

--- Introduccion a IoT/Proyecto/pyfiles/esp32Tools.py
+++ Introduccion a IoT/Proyecto/pyfiles/esp32Tools.py
@@ -1,6 +1,10 @@
 import pandas as pd #Import pandas for csv files
 import socket #Socket will communicate with ESP32 and it sends information
 import time #Sleep time to send information
+from email.message import EmailMessage #Send a email notification
+import Credentials #Passwords and tokens
+import ssl #Email Certificates
+import smtplib #Email protol (SMTP)
 #Class esp32Tools get most important
 class esp32Tools:
     def __init__(self,ip):
@@ -9,18 +13,45 @@ class esp32Tools:
         self.ipdst = "" #IP Destination
         self.lengthsize = 0 #Size
         self.protocol = "" #Protocol
-        self.greenRange = 100 #Green Led will turn on 
-        self.yellowRange = 150#Yellow Led will turn on 
-        self.redRange = 200#Red Led will turn on 
+        self.greenRange = 100 #Green Led will turn on
+        self.yellowRange = 150#Yellow Led will turn on
+        self.redRange = 200#Red Led will turn on
         self.esp32Port = 80 #Port for webconnexion
-class csvFiles(esp32Tools):
-    def __init__(self,ip,name):
+
+class emailSender(esp32Tools):
+    def __init__(self, ip):
+        super().__init__(ip)
+        self.emailSender = Credentials.AUTHOR_EMAIL
+        self.emailPassword = Credentials.AUTHOR_PASS
+        self.emailReceiver = Credentials.RECIPIENT_EMAIL
+        self.smtpServer = Credentials.SMTPSERVER
+        self.subject = ""
+        self.body   = ""
+        self.smtpProtocol = 465
+        self.certificate = ssl.create_default_context()
+    def subjectGet(self,subject):
+        self.subject = subject
+        return self.subject
+    def bodyGet(self,body):
+        self.body = body
+        return self.body
+    def sendEmail(self):
+        email = EmailMessage()
+        email['From'],email["To"],email["subject"] = self.emailSender,self.emailReceiver,self.subject
+        email.set_content(self.body)
+        with smtplib.SMTP_SSL(self.smtpServer,self.smtpProtocol,context=self.certificate) as smtpService:
+            smtpService.login(self.emailSender,self.emailPassword)
+            smtpService.sendmail(self.emailSender,self.emailReceiver,email.as_string())
+
+class csvFiles(emailSender):
+    def __init__(self, ip,name):
         super().__init__(ip)
         self.nameFile = name #File name csv
         self.source= "" #Path from csv
         self.columns = "" #Number of columns
         self.rows = "" #Number of rows
         self.data = pd.read_csv(self.nameFile) #Read csv using Pandas
+
     def isFileCSV(self): #Method: Check if it is a csv file
         self.nameFile = str(self.nameFile) #Get the path
         if self.nameFile.lower().endswith('.csv'): #Compare .csv file
@@ -34,7 +65,7 @@ class csvFiles(esp32Tools):
         self.rows = self.data.shape[0] #Get rows (Number)
         if self.columns==7:
             try: #If it fails
-                print("Delete ID column") 
+                print("Delete ID column")
                 self.data.drop("No.",inplace=True,axis=1) #Delete column "No."
                 return self.data #Return data
             except:
@@ -44,41 +75,47 @@ class csvFiles(esp32Tools):
 
 
 class esp32Data(csvFiles): #Subclass esp32Data sends
-    def __init__(self,ip,name):
+    def __init__(self, ip,name):
         super().__init__(ip,name)
         self.messageSign = "" #Values for leds to ESP32
-        self.messagePort = "" #Message from csv
-    def openSocket(self,typeMessage,message): #Open a socket
+        self.messagePort = ""#Local message
+    def openSocket(self,typeMessage): #Open a socket
         self.messageSign = typeMessage
-        self.messagePort =message
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((self.ipget, self.esp32Port))
-        client_socket.sendall(self.messageSign.encode()) #Sends (0 or 1 or -1)
-        time.sleep(1)
-        client_socket.sendall(self.messagePort.encode()) #Sends message
-        time.sleep(1)
-        client_socket.close()
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((self.ipget, self.esp32Port))
+            client_socket.sendall(self.messageSign.encode()) #Sends (0 or 1 or -1)
+            client_socket.close()
+        except:
+            print("Check your ESP32 IP")
     def systemAlarm(self):
-        self.data = self.removeAdditionalID()
+        try:
+            self.data = self.removeAdditionalID()
+        except:
+            pass
         for self.ipsrc ,self.ipdst,self.protocol,self.lengthsize in zip(self.data["Source"],self.data['Destination'],self.data["Protocol"],self.data["Length"]): #loop for values from csv file
+            time.sleep(4)
             if(self.lengthsize<=self.greenRange):
-                time.sleep(3)
                 self.messageSign = "0" #Sends a 0
-                self.messagePort = "Everything's fine"#Sends message "OK"
-                self.openSocket(self.messageSign,self.messagePort)
-                time.sleep(3)
+                self.openSocket(self.messageSign)
+                self.messagePort = "Everything's Fine"
                 print(self.messagePort)
+                time.sleep(6)
             elif(self.lengthsize>self.greenRange and self.lengthsize<=self.yellowRange):
-                time.sleep(3)
                 self.messageSign = "1"#Sends a 1. Sends message "size of packet"
                 self.messagePort = "Warning! Length packet: "+str(self.lengthsize)
-                self.openSocket(self.messageSign,self.messagePort)
-                time.sleep(3)
+                self.openSocket(self.messageSign)
                 print(self.messagePort)
+                time.sleep(6)
             elif(self.lengthsize>=self.redRange or self.lengthsize>self.yellowRange):
-                time.sleep(3)
                 self.messageSign = "-1"#Sends a -1. Sends message "ipsrc and ipdst"
-                self.messagePort = str(self.ipsrc)+"==>"+str(self.ipdst)+".Protocol: "+str(self.protocol)
-                self.openSocket(self.messageSign,self.messagePort)
-                time.sleep(3)
+                subjectMessage = "SECURITY ALERT LOG!"
+                bodyMessageComplete = "Hi,{}.\nThere was a problem with you network. It's been registered high traffic from the IPs {} to {}. Protocol: {}.\nPlease login into local session to check your traffic of server ESP32.".format(Credentials.RECIPIENT_EMAIL.split("@")[0],self.ipsrc,self.ipdst,self.protocol)
+                self.messagePort = "High log!"
+                esp32Data.subjectGet(self,subjectMessage)
+                esp32Data.bodyGet(self,bodyMessageComplete)
+                esp32Data.sendEmail(self)
+                self.openSocket(self.messageSign)
                 print(self.messagePort)
+                time.sleep(6)
+
