a26cba0f3c026d4f0684618a3748b66df8457ce2
JohnKun136NVCP <johnb@ciencias.unam.mx>
2023-11-22 20:45:16 -0600

    [FIXED][DEV][COLLABORATOR]
    - Deleted $email parameter from database

--- IntroduccionIoT/Proyecto/website/conection.php
+++ IntroduccionIoT/Proyecto/website/conection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "root";
 $dbname = "ids";
 
-$conn = new mysqli($servername, $username, $password,$email, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
